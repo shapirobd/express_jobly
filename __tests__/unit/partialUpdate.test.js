571a35abe8e3e378bacd26fddb4d73291749514d
Brian Shapiro <briandavidshapiro@gmail.com>
2020-12-11 01:23:19 -0500

    companies table created

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,13 @@
+const sqlForPartialUpdate = require("partialUpdate.js");
+
 describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
-
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
-
-  });
+	it("should generate a proper partial update query with just 1 field", () => {
+		// FIXME: write real tests!
+		const table;
+		const items;
+		const key;
+		const id;
+		const queryValues = sqlForPartialUpdate(table, items, key, id);
+		expect(queryValues).toEqual(expect().any(Object));
+	});
 });
