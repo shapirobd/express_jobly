0af04c6ef2aafe6490546d1274a2e4ea5b2db816
Brian Shapiro <briandavidshapiro@gmail.com>
2020-12-15 18:36:04 -0500

    created users table

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS companies CASCADE;
 DROP TABLE IF EXISTS jobs CASCADE;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE companies (
     handle TEXT PRIMARY KEY,
@@ -23,3 +24,16 @@ CREATE TABLE jobs (
 
 INSERT INTO jobs (title, salary, equity, company_handle) VALUES ('Software Developer', 50000.00, 0.5, 'GOOG');
 INSERT INTO jobs (title, salary, equity, company_handle) VALUES ('CEO', 150000.00, 0.75, 'AAPL');
+
+CREATE TABLE users (
+    username TEXT PRIMARY KEY,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    photo_url TEXT,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+INSERT INTO users (username, password, first_name, last_name, email, photo_url, is_admin) VALUES ('shapirobd', 'Pot3ntiat321!', 'Brian', 'Shapiro', 'briandavidshapiro@gmail.com', 'this is a photo url', true);
+INSERT INTO users (username, password, first_name, last_name, email, is_admin) VALUES ('nickgriffo', 'ireek123', 'Nick', 'Griffo', 'nickcg@gmail.com', false);
